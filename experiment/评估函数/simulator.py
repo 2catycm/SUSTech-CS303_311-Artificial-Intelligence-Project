3b31e5c808bf56489a0ae6e063253ea649de9295
叶璨铭 <12011404@mail.sustech.edu.cn>
2022-07-12 20:05:23 +0800

    feat(simulator): 升级为新版numba 来更新棋盘，加快simulator速度

--- experiment/评估函数/simulator.py
+++ experiment/评估函数/simulator.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-# import src.project1.submit.AI as ai
-import experiment.old_ai.AI as ai
+import src.project1.submit.AI as ai
+# import experiment.old_ai.AI as ai
 
 import matplotlib.pyplot as plt
 
@@ -22,7 +22,6 @@ class Simulator:
     def __init__(self, chessboard_size, time_out, agents, current_color=ai.COLOR_BLACK):
         self.chessboard_size = chessboard_size
         self.time_out = time_out
-        self.reversi_env = ai.ReversiEnv(chessboard_size)
         self.current_color = current_color
         self.current_chessboard = Simulator.init_chessboard(chessboard_size)
         self.agents = agents  # color 映射 对象 的dict
@@ -32,32 +31,29 @@ class Simulator:
         if accountants is None:
             accountants = []
         self.rounds = 4
-        while not self.reversi_env.is_terminal(self.current_chessboard, self.current_color):
+        while not ai.is_terminal(self.current_chessboard, self.current_color):
             if not no_print:
                 print(f"Round {self.rounds}")
             # 注意落子空闲，我们定义没有行动力的时候回合不会增加
-            black, white = self.reversi_env.piece_cnt(self.current_chessboard)
-            if self.rounds!= black+white:
+            black, white = ai.piece_cnt(self.current_chessboard)
+            if self.rounds != black + white:
                 raise AssertionError
 
             for accountant in accountants:
                 accountant(self.rounds, self.current_color, self.current_chessboard, self.agents)
-            self.quick_step()
-            # yield self.current_chessboard
-            # plt.imshow(self.current_chessboard)
-            # plt.pause(0.01)
-            # plt.show()
+            self.quick_step(no_print)
 
-        return self.reversi_env.get_winner(self.current_chessboard)
+        return ai.get_winner(self.current_chessboard)
 
-    def quick_step(self):
+    def quick_step(self, no_print=False):
         agent = self.agents[self.current_color]
         agent.go(self.current_chessboard)
         if len(agent.candidate_list) != 0:
-            self.current_chessboard = self.reversi_env.updated_chessboard(self.current_chessboard, self.current_color,
-                                                                          agent.candidate_list[-1])
+            self.current_chessboard = ai.updated_chessboard(self.current_chessboard, self.current_color,
+                                                            np.array(agent.candidate_list[-1]))
             self.rounds += 1
         else:
-            print("过")
+            if not no_print:
+                print("过")
 
         self.current_color *= -1

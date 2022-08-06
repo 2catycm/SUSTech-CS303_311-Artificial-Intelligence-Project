0ee5af707e276595d8e3a432def01db690fba0e9
叶璨铭 <12011404@mail.sustech.edu.cn>
2022-08-06 20:58:50 +0800

    feat(project2): 增加本地评测机

---
+++ src/project3_容量限制约束弧路径问题/test/local_judge.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+import os
+import time
+
+time_limit_seconds = 60
+random_seed = 0
+solver_location = "../submit/CARP_solver.py"
+data_location = "CARP_samples"
+if __name__ == '__main__':
+    cwd = Path('.')
+    cwd = cwd.absolute()
+    cwd /= data_location
+    costs = []
+    dataset_names = []
+    times = []
+    dats = list(cwd.rglob("*.dat"))
+    for i, dat in enumerate(dats):
+        name = dat.stem.split("_")[1]  # 要求命名按照 i_name 的形式。
+        print(f"Running {i}/{len(dats) - 1} instance {name}. ")
+        param = f'"{dat}" '
+        param += f'-t {time_limit_seconds} '
+        param += f'-s {random_seed} '
+        with os.popen(f'python "{solver_location}" {param}', 'r') as p:
+            start = time.time()
+            result = p.readlines()  # 这一步就结束了
+            time_used = time.time() - start
+            times.append(time_used)
+            dataset_names.append(name)
+            cost = int(result[1].split()[1])
+            costs.append(cost)
+            print(f"Instance {name} finished with time {time_used:e}s and cost {cost}.")
+
+    print(dataset_names)
+    print(costs)
+    print(f"Total cost is {sum(costs)}. Total time used is {sum(times)}s. ")

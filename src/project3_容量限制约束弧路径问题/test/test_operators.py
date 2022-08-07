ab55148048b398bd3f8c78e3b03d48513c81fd1c
叶璨铭 <12011404@mail.sustech.edu.cn>
2022-08-07 11:33:19 +0800

    feat(project2): 增加简单烟雾测试

--- src/project3_容量限制约束弧路径问题/test/test_operators.py
+++ src/project3_容量限制约束弧路径问题/test/test_operators.py
@@ -28,6 +28,31 @@ class MyTestCase(unittest.TestCase):
             routes, costs = solution_operators.ulusoy_split(task_edges, carp_instance)
             self.assertEqual(carp_instance.costs_of(routes), costs)
 
+    def test_can_operate(self):
+        for i, dat in enumerate(dats):
+            carp_instance = CarpInstance().with_file(str(dat)).with_distances_calculated()
+            carp_solver = HeuristicSearch(carp_instance)
+            solution = carp_solver.path_scanning()
+            task_edges_old = solution_operators.merge(solution.routes)
+            task_edges_new, i, j = solution_operators.operator_interface('single_insertion', task_edges_old)
+            if i!=j:
+                self.assertNotEqual(task_edges_new, task_edges_old)
+
+            task_edges_old = task_edges_new
+            task_edges_new, i, j = solution_operators.operator_interface('double_insertion', task_edges_old)
+            if i!=j:
+                self.assertNotEqual(task_edges_new, task_edges_old)
+
+            task_edges_old = task_edges_new
+            task_edges_new, i, j = solution_operators.operator_interface('swap', task_edges_old)
+            if i != j:
+                self.assertNotEqual(task_edges_new, task_edges_old)
+
+            task_edges_old = task_edges_new
+            task_edges_new, i, j = solution_operators.operator_interface('flip', task_edges_old) # 这一步要求deep copy
+            if i != j:
+                self.assertNotEqual(task_edges_new, task_edges_old)
+
 
 if __name__ == '__main__':
     unittest.main()
